92b9c2368185d2037bf2557579dde1b571068641
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-04-21 01:27:58 +0500

    some changes in 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center bg-gray-100'>
-        <div className='text-center'>
-            <h1 className='text-4xl font-bold mb-4'>404</h1>
-            <p className='text-xl text-gray-600 mb-4'>Oops! page not found</p>
-            <Link to={"/"} className='text-blue-500 hover:text-blue-700 underline'>Return to home</Link>
-        </div>
-
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404</h1>
+        <p className="text-xl text-gray-600 mb-4">Oops! page not found</p>
+        <Link to={"/"} className="text-blue-500 hover:text-blue-700 underline">
+          Return to home
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
